a5f64a95c8ac71c8c6c974e1fc4dafdcbd33ef12
kwolfski <kt.schramm.design@gmail.com>
2017-10-01 21:39:29 -0500

    Final

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ var user = window.prompt("What's your name?");
 
 
 
-    //When the page loads, use the window.prompt() command to ask for the user’s name. When the user responds, use a conditional to check that the type of the returned value is a string and that it is not equal to a blank string
-    //(a blank string in JavaScript is expressed like this “”). If the user has entered a non-blank string, display this in the console: Hi [user’s name]!
+//When the page loads, use the window.prompt() command to ask for the user’s name. When the user responds, use a conditional to check that the type of the returned value is a string and that it is not equal to a blank string
+//(a blank string in JavaScript is expressed like this “”). If the user has entered a non-blank string, display this in the console: Hi [user’s name]!
 
     if (typeof user === "string" && user.length !== 1) {
       console.log("Hi " + user + "!");
-    }
+      }
     else {
       console.log("OK, I'll just call you User.");
-    };
+};
 
     //You may notice that a new property of “authorAge” has been added to each of the speech objects. We are going to use the “Donate” button to ask the user which author is their favorite and then display the age of the specified author.
     //To do this, start by using another window.prompt() command to ask the user’s favorite author when the “Donate” button is clicked. Then use a SWITCH statement to check which author name they entered.
@@ -125,7 +125,7 @@ document.getElementById('BtnGhandi').addEventListener('click', function(){
       console.log("This is the most recent speech on the page.");
     }
     else {
-      console.log("This is the middle speech");
+      console.log("This is the middle speech.");
     }
 
 });
